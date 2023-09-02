2aab0492471faa7466880b6021d8a3fe9b932d62
abudaan <daniel@abumarkub.net>
2023-09-02 15:27:49 +0200

    cleanup repo

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Storage } from "./Storage";
-import { StorageType, IStorage, AdapterConfig, ConfigAmazonS3, ConfigBackblazeB2, ConfigGoogleCloud, ConfigLocal } from "./types";
-export { Storage, IStorage, StorageType, AdapterConfig, ConfigAmazonS3, ConfigBackblazeB2, ConfigGoogleCloud, ConfigLocal, };
